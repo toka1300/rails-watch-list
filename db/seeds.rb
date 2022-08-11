a1d2e75d59f7fd5b69e6967b1499f74d67a44807
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-11 14:23:29 -0400

    dsf

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Emptying the database!"
+
+Movie.destroy_all
+
+puts "Adding movies to the database..."
+
+10.times do
+  Movie.create(title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: "https://picsum.photos/200/300",
+    rating: rand(0.1...5).round(2))
+end
+
+puts "I got your silly movies for you, you are very welcome!"
