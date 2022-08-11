bf5306357e36c4e852570715d9785293f547a89e
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-11 15:33:25 -0400

    dfgedg

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ puts "Adding movies to the database..."
 
 10.times do
   Movie.create(title: Faker::Movie.title,
-    overview: Faker::Movie.quote,
+    overview: Faker::TvShows::MichaelScott.quote,
     poster_url: "https://picsum.photos/200/300",
     rating: rand(0.1...5).round(2))
 end
