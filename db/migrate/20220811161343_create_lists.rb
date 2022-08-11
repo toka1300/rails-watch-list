182522f2d3112c80c3af8b1aaa1d2ef720cfd18b
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-11 14:02:20 -0400

    done

---
+++ db/migrate/20220811161343_create_lists.rb
@@ -0,0 +1,8 @@
+class CreateLists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lists do |t|
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20220811162958_add_name_column_to_lists.rb
@@ -0,0 +1,5 @@
+class AddNameColumnToLists < ActiveRecord::Migration[7.0]
+  def change
+    add_column :lists, :name, :string
+  end
+end
