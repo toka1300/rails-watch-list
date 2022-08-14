0808fdb02434aaeb7dce6eaf04d75624ef6e9654
casey tokarchuk <caseytokarchuk@gmail.com>
2022-08-14 17:13:31 -0400

    delete

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -20,6 +20,12 @@ class BookmarksController < ApplicationController
     @bookmarks = Bookmark.all
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
   private
 
   def bookmark_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, only: [:show, :index, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :delete]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
